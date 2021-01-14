db60f3424724e82e2a2a35e83a8827a01fcc5825
JMan <justinvang07@gmail.com>
2021-01-14 14:50:21 -0600

    first commit, created functional button

--- Assets/script.js
+++ Assets/script.js
@@ -1,1 +1,33 @@
-//write js code here
+// food recipe search function
+$("#recipeButton").on("click", function (event) {
+    event.preventDefault()
+    var foodKeyword = $("#RecipeName").val();
+var queryURL = "https://api.edamam.com/search?q=" + foodKeyword + "&app_id=254ee167&app_key=2753a296392ca0118d13115188aa926c";
+
+$.ajax({
+    url: queryURL,
+    method: "GET"
+}).then(function (response) {
+    console.log(response);
+    for (let i = 0; i < response.hits.length; i++) {
+        var recList = response.hits[i];
+        console.log(recList.recipe.label);
+        console.log(recList.recipe.ingredientLines);
+        console.log(recList.recipe.image);
+    // $("#recipeInfo").text(JSON.stringify(response)); --> to display items (currently only showing words)
+    }
+});
+
+
+// Drink API get function
+var drinkKeyword = "margarita";
+var queryURL = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=" + drinkKeyword;
+
+$.ajax({
+    url: queryURL,
+    method: "GET"
+}).then(function (response) {
+    console.log(response);
+});
+
+});
