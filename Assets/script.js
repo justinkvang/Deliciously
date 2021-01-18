47d8feeb00191a917f3f3c4574775044ba9293ed
Snow Day <jakedoty@gmail.com>
2021-01-18 14:23:08 -0600

    drink list

--- Assets/script.js
+++ Assets/script.js
@@ -10,40 +10,40 @@
 $("#searchBtn").on("click", function (event) {
     event.preventDefault();
     var foodKeyword = $(".searchTerm").val();
-var queryURL = "https://api.edamam.com/search?q=" + foodKeyword + "&app_id=254ee167&app_key=2753a296392ca0118d13115188aa926c";
+    var queryURL = "https://api.edamam.com/search?q=" + foodKeyword + "&app_id=254ee167&app_key=2753a296392ca0118d13115188aa926c";
 
-$.ajax({
-    url: queryURL,
-    method: "GET"
-}).then(function (response) {
-    console.log(response);
-    for (let i = 0; i < response.hits.length; i++) {
-        var recList = response.hits[i];
-        // const ingredArr = []; I need to populate an array from here that can be defined in a variable
-        // let ingredArr = $(recList.recipe.ingredientLines);
-        // console.log(recList);
-        console.log(recList.recipe.label);
-        console.log(recList.recipe.ingredientLines);
-        console.log(recList.recipe.image);
-        // $("#recipeInfo").text(JSON.stringify(response)); --> to display items (currently only showing words)
-
-    }
-    // var queryURL = "https://api.edamam.com/search?q=" + foodKeyword + "&app_id=254ee167&app_key=2753a296392ca0118d13115188aa926c";
-
-    // $.ajax({
-    //     url: queryURL,
-    //     method: "GET"
-    // }).then(function (response) {
-    //     console.log(response);
-    //     for (let i = 0; i < response.hits.length; i++) {
-    //         var recList = response.hits[i];
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response);
+        for (let i = 0; i < response.hits.length; i++) {
+            var recList = response.hits[i];
             // const ingredArr = []; I need to populate an array from here that can be defined in a variable
             // let ingredArr = $(recList.recipe.ingredientLines);
             // console.log(recList);
-            // console.log(recList.recipe.label);
-            // console.log(recList.recipe.ingredientLines);
-            // console.log(recList.recipe.image);
+            console.log(recList.recipe.label);
+            console.log(recList.recipe.ingredientLines);
+            console.log(recList.recipe.image);
             // $("#recipeInfo").text(JSON.stringify(response)); --> to display items (currently only showing words)
+
+        }
+        // var queryURL = "https://api.edamam.com/search?q=" + foodKeyword + "&app_id=254ee167&app_key=2753a296392ca0118d13115188aa926c";
+
+        // $.ajax({
+        //     url: queryURL,
+        //     method: "GET"
+        // }).then(function (response) {
+        //     console.log(response);
+        //     for (let i = 0; i < response.hits.length; i++) {
+        //         var recList = response.hits[i];
+        // const ingredArr = []; I need to populate an array from here that can be defined in a variable
+        // let ingredArr = $(recList.recipe.ingredientLines);
+        // console.log(recList);
+        // console.log(recList.recipe.label);
+        // console.log(recList.recipe.ingredientLines);
+        // console.log(recList.recipe.image);
+        // $("#recipeInfo").text(JSON.stringify(response)); --> to display items (currently only showing words)
         // }
 
     });
@@ -59,20 +59,48 @@ $.ajax({
 });
 
 
-$("#searchBtn").on("click", function (event) {
-    event.preventDefault();
-    var recipeNu = $(".searchTerm").val();
-var queryURL = "https://api.edamam.com/api/nutrition-details&app_id=17f53caf&app_key=5f7c6577003274ea3648841df623a1ba";
+// $("#searchBtn").on("click", function (event) {
+//     event.preventDefault();
+//     var recipeNu = $(".searchTerm").val();
+// var queryURL = "https://api.edamam.com/api/food-database/v2/parser?ingr=" + recipeNu + "&app_id=a4545788&app_key=a7226685508b47e25af9e7e4c9e1a12b";
+
+// $.ajax({
+//     url: queryURL,
+//     method: "GET",
+//     // Content-Type: application/json
+// }).then(function (response) {
+//     console.log(response);
+//     // for (let i = 0; i < response.hits.length; i++) {
+//     //     var recList = response.hits[i];
+//     // }        
+
+// });
+// });
+var drinkKeyword = "margarita";
+var queryURL = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=" + drinkKeyword;
 
 $.ajax({
     url: queryURL,
     method: "GET"
-    Content-Type: application/json
 }).then(function (response) {
-    console.log(response);
-    for (let i = 0; i < response.hits.length; i++) {
-        var recList = response.hits[i];
-    }        
+    // To use this api we need to create a function that digs through an 
+    // object within an array to pull ingredients and measurements without pulling the null values.
+    for (let i = 0; i < response.drinks.length; i++) {
+        var drinkNum = response.drinks[i];
+        console.log(drinkNum.strIngredient1);
+        console.log(drinkNum);
 
-});
+        for (let il = 1; il < 16; il++) {
+            console.log(response.il);
+
+        }
+        // for (let ind = 0; ind < $(drinkNum.length); ind++) {
+        //     var strIngredArr = drinkNum.strIngredient[ind];
+        //     console.log(strIngredArr);
+        //     console.log(drinkNum);
+
+
+
+    }
+
 });
